a95a4ed0eefda4b1c009b02e62f2a16a96b2059a
Mark Moll <mdmoll@gmail.com>
2019-03-17 22:04:44 -0400

    Fixed gradebook

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -7,47 +7,69 @@ import java.util.List;
 public class Classroom {
     private Student[] students;
     List<Student> studentList = new ArrayList<>(Arrays.asList(students));
-
-    public Classroom(){
+    
+    public Classroom classroom = new Classroom();
+    
+    public Classroom() {
         this.students = new Student[30];
     }
-
-    public Classroom(int maxNumberOfStudents){
+    
+    public Classroom(int maxNumberOfStudents) {
         this.students = new Student[maxNumberOfStudents];
     }
-
-    public Classroom(Student[] students){
+    
+    public Classroom(Student[] students) {
         this.students = students;
     }
-
+    
     public Student[] getStudents() {
         return students;
     }
-
+    
     public double getAverageExamScore() {
         double sum = 0.0;
         for (Student student : students) {
-                sum += student.examSum();
-            } return sum / students.length;
+            sum += student.examSum();
         }
-
-    public void addStudent(Student student){
+        return sum / students.length;
+    }
+    
+    public void addStudent(Student student) {
         studentList.add(student);
         students = studentList.toArray(new Student[0]);
     }
-
-    public void removeStudent(String firstName, String lastName){
+    
+    public void removeStudent(String firstName, String lastName) {
         studentList.removeIf(obj -> obj.getFirstName().equals(firstName) && obj.getLastName().equals(lastName));
         students = studentList.toArray(students);
     }
-
-    public Student[] getStudentsbyScore(){
-        return Arrays.sort(studentList);
+    
+    public String[] getStudentsByScore() {
+        List<Student> studentList = new ArrayList<>(Arrays.asList(this.students));
+        List<String> examList = new ArrayList<>();
+        for (Student student : studentList) {
+            String examScores = student.getExamScores();
+            examList.add(examScores);
+        }
+        return examList.toArray(new String[0]);
     }
-
-   /* public Map<Students, String> getGradeBook(){
-
-    }*/
-
-    public Classroom classroom = new Classroom();
+    
+    public String getGradebook() {
+        List<Student> studentList = new ArrayList<>(Arrays.asList(this.students));
+        StringBuilder result = new StringBuilder();
+        for (Student student : studentList) {
+            if (student.getAverageExamScore() < 60) {
+                result.append(student.getFirstName()).append(": F");
+            } else if (student.getAverageExamScore() < 71) {
+                result.append(student.getFirstName()).append(": D");
+            } else if (student.getAverageExamScore() < 81) {
+                result.append(student.getFirstName()).append(": C");
+            } else if (student.getAverageExamScore() < 91) {
+                result.append(student.getFirstName()).append(": B");
+            } else {
+                result.append(student.getFirstName()).append(": A");
+            }
+        }
+        return result.toString();
+    }
 }
